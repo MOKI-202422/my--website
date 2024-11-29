100f2ea16d5df38de081288d29020a44b9cb79f7
MOKI-202422 <t21j090zi@ous.jp>
2024-11-29 16:34:07 +0900

    Update server to use dynamic PORT

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const port = 3320;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 app.use(express.static("public"));
 
